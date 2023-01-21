85bf660998d2797aec0dcf606f98d4a55685af9e
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2023-01-20 23:20:30 -0300

    adicionando parent_id para comentario

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_13_000413) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_21_021547) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_13_000413) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "parent_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
